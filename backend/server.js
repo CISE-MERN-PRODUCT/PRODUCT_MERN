bb99d5bebdaae59159e5f7bef7f436b43d690b62
MrGeet <aldarbatomunkuev@gmail.com>
2021-04-22 14:59:58 +1200

    sup

--- backend/server.js
+++ backend/server.js
@@ -31,4 +31,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log('Server is running on the port 8082'));
+app.listen(port, console.log('Server is running on the port 8000'));
