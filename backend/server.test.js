7da7ac18508b3c648e1195217df7e5f89f677887
MrGeet <aldarbatomunkuev@gmail.com>
2021-05-13 11:17:30 +1200

    test backend

--- backend/server.test.js
+++ backend/server.test.js
@@ -1,15 +1,33 @@
 const request = require('supertest');
 const articleRoutes = require('./routes/articles');
 const practiceRoutes = require('./routes/practices');
+const express = require('express');
 
+const app = require('./app');
+beforeAll((done) => {
+  done();
+});
 describe('practice routes', () => {
-  it('GET /articles --> array of practices', () => {
-    return request(practiceRoutes)
-      .get('/')
+  it('GET /practice --> array of practices', async (done) => {
+    return request(app)
+      .get('/api/practices/')
       .expect('Content-Type', /json/)
       .expect(200)
       .then((response) => {
-        expect(response.body).toEqual(expect.array);
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: expect.any(String),
+            }),
+          ]),
+        );
+        done();
       });
+    // expect(response.statusCode).toBe(200);
   });
+});
+
+afterAll((done) => {
+  // Closing the DB connection allows Jest to exit successfully.
+  done();
 });
